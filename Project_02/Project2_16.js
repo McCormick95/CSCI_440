b1004fdee9760851d4df5d49f0340c0bcebcb4e4
McCormick95 <mccormick.ryan.95@gmail.com>
2024-09-28 20:44:47 -0400

    Nebula and Star are working

--- Project_02/Project2_16.js
+++ Project_02/Project2_16.js
@@ -8,26 +8,31 @@ var numPositions  = 36;
 var positions = [];
 var colors = [];
 
-var xAxis = 0;
-var yAxis = 1;
-var zAxis = 2;
+var m_Matrix;
+var MatrixLoc;
 
-var axis = 0;
-var theta = [0, 0, 0];
+var  angle = 0.0;
+var  axis = vec3(0, 0, 1);
 
-var thetaLoc;
+var lastPos = [0, 0, 0];
+var curx, cury;
+var startX, startY;
+
+var status_u = -1;
+
+
 
 window.onload = function init()
 {
-    canvas = document.getElementById("gl-canvas");
+    canvas = document.getElementById( "gl-canvas" );
 
     gl = canvas.getContext('webgl2');
-    if (!gl) alert( "WebGL 2.0 isn't available");
+    if (!gl) { alert( "WebGL 2.0 isn't available" ); }
 
     colorCube();
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
     gl.enable(gl.DEPTH_TEST);
 
@@ -35,15 +40,15 @@ window.onload = function init()
     //  Load shaders and initialize attribute buffers
     //
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram(program);
+    gl.useProgram( program );
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 
-    var colorLoc =gl.getAttribLocation( program, "aColor");
-    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(colorLoc);
+    var colorLoc = gl.getAttribLocation( program, "aColor");
+    gl.vertexAttribPointer( colorLoc, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( colorLoc );
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
@@ -52,20 +57,31 @@ window.onload = function init()
 
     var positionLoc = gl.getAttribLocation( program, "aPosition");
     gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
+    gl.enableVertexAttribArray(positionLoc );
 
-    thetaLoc = gl.getUniformLocation(program, "uTheta");
+    m_Matrix = mat4();
+    MatrixLoc = gl.getUniformLocation(program, "uMatrix");
+    
+    //gl.uniformMatrix4fv(rotationMatrixLoc, false, flatten(rotationMatrix));
 
-    //event listeners for buttons
+    document.getElementById("settings").onclick = function() {
+        var selectedOption = this.options[this.selectedIndex];
+        alert("Selected: " + selectedOption.text);
 
-    document.getElementById( "xButton" ).onclick = function () {
-        axis = xAxis;
-    };
-    document.getElementById( "yButton" ).onclick = function () {
-        axis = yAxis;
-    };
-    document.getElementById( "zButton" ).onclick = function () {
-        axis = zAxis;
+        if(selectedOption.value == 1){
+            status_u = 1;
+            
+        }
+        else if(selectedOption.value == 2){
+            status_u = 2;
+        }
+        else if(selectedOption.text == 3){
+            status_u = 2;
+        }
+        else{
+            status_u = 0;
+            
+        }
     };
 
     render();
@@ -73,36 +89,36 @@ window.onload = function init()
 
 function colorCube()
 {
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 0, 1);
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
 }
 
 function quad(a, b, c, d)
 {
     var vertices = [
-        vec4(-0.5, -0.5,  0.5, 1.0),
-        vec4(-0.5,  0.5,  0.5, 1.0),
-        vec4(0.5,  0.5,  0.5, 1.0),
-        vec4(0.5, -0.5,  0.5, 1.0),
-        vec4(-0.5, -0.5, -0.5, 1.0),
-        vec4(-0.5,  0.5, -0.5, 1.0),
-        vec4(0.5,  0.5, -0.5, 1.0),
-        vec4(0.5, -0.5, -0.5, 1.0)
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5, -0.5, -0.5, 1.0 )
     ];
 
     var vertexColors = [
-        vec4(0.0, 0.0, 0.0, 1.0),  // black
-        vec4(1.0, 0.0, 0.0, 1.0),  // red
-        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
-        vec4(0.0, 1.0, 0.0, 1.0),  // green
-        vec4(0.0, 0.0, 1.0, 1.0),  // blue
-        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
-        vec4(0.0, 1.0, 1.0, 1.0),  // cyan
-        vec4(1.0, 1.0, 1.0, 1.0)   // white
+      vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+      vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+      vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+      vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+      vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+      vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+      vec4( 0.0, 1.0, 1.0, 1.0 ),  // cyan
+      vec4( 1.0, 1.0, 1.0, 1.0 )   // white
     ];
 
     // We need to parition the quad into two triangles in order for
@@ -111,15 +127,20 @@ function quad(a, b, c, d)
 
     //vertex color assigned by the index of the vertex
 
-    var indices = [a, b, c, d];
+    var indices = [ a, b, c, d];
 
     for ( var i = 0; i < indices.length; ++i ) {
         positions.push(vertices[indices[i]]);
-        //colors.push( vertexColors[indices[i]] );
-        
-        // for solid colored faces use
-        colors.push(vertexColors[a]);
 
+        if(status_u == 1){
+            // for interpolated colors use
+            colors.push(vertexColors[indices[i]]);
+            
+        }
+        else{
+            // for solid colored faces use
+            colors.push(vertexColors[a]);
+        }
     }
 }
 
@@ -127,16 +148,26 @@ function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    theta[axis] += 0.05;
-    gl.uniform3fv(thetaLoc, theta);
+    //m_Matrix = mult(translate(0.025, .025, 0), rotate(angle, axis));
 
-    gl.drawArrays(gl.LINE_LOOP, 0, 4);
-    gl.drawArrays(gl.LINE_LOOP, 4, 4);
-    gl.drawArrays(gl.LINE_LOOP, 8, 4);
-    gl.drawArrays(gl.LINE_LOOP, 12, 4);
-    gl.drawArrays(gl.LINE_LOOP, 16, 4);
-    gl.drawArrays(gl.LINE_LOOP, 20, 4);
+    m_Matrix = mult(m_Matrix, rotate(angle, axis));
 
+    gl.uniformMatrix4fv(MatrixLoc, false, flatten(m_Matrix));
 
+    if(status_u == 1){
+        axis = [1, 1, 0];
+        angle = Math.PI / 4;
+        for( var i=0; i<positions.length; i+=4){
+            gl.drawArrays( gl.TRIANGLE_FAN, i, 4);
+        }
+    }
+    else{
+        axis = [1, 1, 0];
+        angle = Math.PI / 4;
+        for( var i=0; i<positions.length; i+=4){
+            gl.drawArrays( gl.LINE_LOOP, i, 4);
+        }
+    }
+    
     requestAnimationFrame( render );
 }
