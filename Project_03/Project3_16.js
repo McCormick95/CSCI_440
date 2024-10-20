1199a29a5404db6aacd3be52d4d6b8bfec00a9e2
McCormick95 <mccormick.ryan.95@gmail.com>
2024-10-20 14:35:52 -0400

    Commented code, removed console.log() statements, moved variables to top of file and adjusted pulse values.

--- Project_03/Project3_16.js
+++ Project_03/Project3_16.js
@@ -2,11 +2,8 @@
 
 var canvas;
 var gl;
-
 var numTimesToSubdivide = 6;
-
 var index = 0;
-
 var positionsArray = [];
 var normalsArray = [];
 
@@ -16,7 +13,6 @@ var radius = 1.5;
 var theta  = 0.0;
 var phi    = 0.0;
 var dr = 5.0 * Math.PI/180.0;
-
 var left = -3.0;
 var right = 3.0;
 var ytop =3.0;
@@ -27,30 +23,6 @@ var vb = vec4(0.0, 0.942809, 0.333333, 1);
 var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
 var vd = vec4(0.816497, -0.471405, 0.333333,1);
 
-var lightPosition = vec4(0.0, 0.25, 1.0, 0.0);
-var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
-var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
-var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
-
-var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
-var materialDiffuse = vec4(0.0, 0.7, 0.3, 1.0);
-var materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
-var materialShininess = 20.0;
-
-var ctm;
-var ambientColor, diffuseColor, specularColor;
-
-var modelViewMatrix, projectionMatrix;
-var modelViewMatrixLoc, projectionMatrixLoc;
-
-var ambientProduct, diffuseProduct, specularProduct;
-
-var nMatrix, nMatrixLoc;
-
-var eye;
-var at = vec3(0.0, 0.0, 0.0);
-var up = vec3(0.0, 1.0, 0.0);
-
 // Awakening Variables
 var awaken_status = false;
 var color_one = vec4(0.0, 0.7, 0.3, 1.0);
@@ -58,7 +30,7 @@ var color_two = vec4(0.8, 0.2, 0.4, 1.0);
 
 // Power Surge Variables
 var power_surge_status = false;
-var pulse_speed = 0.007;
+var pulse_speed = 0.01;
 var pulse_toggle = 1;
 var r_max, g_max, b_max;
 var updateDiffuse;
@@ -74,7 +46,26 @@ var new_light_pos;
 var focused_gaze_status = false;
 var focused_gaze_speed = 0.02;
 var focused_gaze_toggle = 1;
-var temp_gaze;
+
+var lightPosition = vec4(0.0, 0.25, 1.0, 0.0);
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+
+var materialAmbient = vec4(1.0, 0.0, 1.0, 1.0);
+var materialDiffuse = color_one;
+var materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+var materialShininess = 20.0;
+
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+var ambientProduct, diffuseProduct, specularProduct;
+var nMatrix, nMatrixLoc;
+var eye;
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
 
 function triangle(a, b, c) {
      positionsArray.push(a);
@@ -161,25 +152,25 @@ window.onload = function init() {
     projectionMatrixLoc = gl.getUniformLocation(program, "uProjectionMatrix");
     nMatrixLoc = gl.getUniformLocation(program, "uNormalMatrix");
 
-    // Awakening
+    // Awakening: button to toggle boolean value to control color change
     document.getElementById("Button0").onclick = function(){
         awaken_status = !awaken_status;
         awaken();
     };
-    // Power Surge
+    // Power Surge: button to toggle boolean value to control pulse effect
     document.getElementById("Button1").onclick = function(){
         power_surge_status = !power_surge_status;
         power_surge(); 
     };
-    // Seeking Light
+    // Seeking Light: button to toggle boolean value to control light movement
     document.getElementById("Button2").onclick = function(){
         seeking_light_status = !seeking_light_status;
         seeking_light();
     };
-    // Focused Gaze
+    // Focused Gaze: button to toggle boolean value to control focused gaze effect
     document.getElementById("Button3").onclick = function(){
         focused_gaze_status = !focused_gaze_status;
-        adjust_projection_matrix();
+        focus_gaze();
     };
 
     gl.uniform4fv(gl.getUniformLocation(program,"uAmbientProduct"),flatten(ambientProduct));
@@ -190,17 +181,17 @@ window.onload = function init() {
 
     render();
 }
-
+// Update the diffuse product
 function updateDiffuseProduct(defuse) {
     diffuseProduct = mult(lightDiffuse, defuse);
     gl.uniform4fv(gl.getUniformLocation(gl.getParameter(gl.CURRENT_PROGRAM), "uDiffuseProduct"), flatten(diffuseProduct));
 }
-
+// Update the light position
 function updateLightPosition(lightPos) {
     lightPosition = lightPos;
     gl.uniform4fv(gl.getUniformLocation(gl.getParameter(gl.CURRENT_PROGRAM), "uLightPosition"), flatten(lightPosition));
 }
-
+// Awakening effect
 function awaken(){
     if(awaken_status){
         materialDiffuse = color_two;
@@ -210,31 +201,36 @@ function awaken(){
     }
     updateDiffuseProduct(materialDiffuse);
 }
-
+// Power Surge effect
 function power_surge(){
     var color_temp = materialDiffuse;
     if (power_surge_status) {
-
+        // Update the color values
         color_temp[0] += pulse_speed * pulse_toggle; 
         color_temp[1] += pulse_speed * pulse_toggle;
         color_temp[2] += pulse_speed * pulse_toggle;
 
-        color_temp[0] = Math.round(color_temp[0] * 1000) / 1000;  
-        color_temp[1] = Math.round(color_temp[1] * 1000) / 1000;
-        color_temp[2] = Math.round(color_temp[2] * 1000) / 1000;
+        // Round the color values to 2 decimal places
+        color_temp[0] = Math.round(color_temp[0] * 100) / 100;  
+        color_temp[1] = Math.round(color_temp[1] * 100) / 100;
+        color_temp[2] = Math.round(color_temp[2] * 100) / 100;
 
+        // Control max value for each color
         r_max = Math.min(1.0, color_temp[0]);
         g_max = Math.min(1.0, color_temp[1]);
         b_max = Math.min(1.0, color_temp[2]);
 
+        // Control min value for each color
         color_temp[0] = Math.max(0.0, r_max);
         color_temp[1] = Math.max(0.0, g_max);
         color_temp[2] = Math.max(0.0, b_max);
 
-        if(color_temp[1] == 1.00 || color_temp[1] == 0.0 || color_temp[2] == 1.0 || color_temp[2] == 0.0 || color_temp[0] == 1.0 || color_temp[0] == 0.0){
+        // Toggle the pulse direction
+        if(color_temp[1] == 1.00 || color_temp[1] == 0.00 || color_temp[2] == 1.00 || color_temp[2] == 0.00 || color_temp[0] == 1.00 || color_temp[0] == 0.00){
             pulse_toggle *= -1;
         }
-
+        
+        // Update the color values
         updateDiffuse = vec4(color_temp[0], color_temp[1], color_temp[2], 1.0);
         updateDiffuseProduct(updateDiffuse); 
         
@@ -244,19 +240,23 @@ function power_surge(){
         return;
     }
 }
-
+// Seeking Light effect
 function seeking_light(){
     var temp_light = lightPosition;
 
     if(seeking_light_status){
+        // Update the light position
         temp_light[0] += light_speed * seek_toggle;
 
+        // Round the light position to 2 decimal places
         temp_light[0] = Math.round(temp_light[0] * 100) / 100;
 
+        // Control max and min values for light position
         if(temp_light[0] == 3.00 || temp_light[0] == -3.00){
             seek_toggle *= -1;
         }
 
+        // Update the light position
         new_light_pos = vec4(temp_light[0], lightPosition[1], lightPosition[2], 0.0);
         updateLightPosition(new_light_pos);
         requestAnimationFrame(seeking_light);
@@ -265,24 +265,23 @@ function seeking_light(){
         return;
     }
 }
-
-function adjust_projection_matrix(){
-    temp_gaze = projectionMatrix;
-
+// Focused Gaze effect
+function focus_gaze(){
     if(focused_gaze_status){
+        // Update the ytop and bottom values
         ytop += focused_gaze_speed * focused_gaze_toggle;
         bottom -= focused_gaze_speed * focused_gaze_toggle;
 
+        // Round values to 2 decimal places
         ytop = Math.round(ytop * 100) / 100;
         bottom = Math.round(bottom * 100) / 100;
 
-        console.log(ytop, bottom);
-
-        if(ytop == 9 || bottom == -9 || ytop == 3 || bottom == -3){
+        // Toggle the direction of the focused gaze effect
+        if(ytop == 7 || bottom == -7 || ytop == 3 || bottom == -3){
             focused_gaze_toggle *= -1;
         }
         
-        requestAnimationFrame(adjust_projection_matrix);
+        requestAnimationFrame(focus_gaze);
     }
     else{
         return;
@@ -304,6 +303,7 @@ function render() {
     requestAnimationFrame(render);
 }
 
+// Render a sphere, given a position
 function render_sphere(position){
     var sphere_position = position;
     eye = vec3(radius * Math.sin(theta) * Math.cos(phi), radius * Math.sin(theta) * Math.sin(phi), radius * Math.cos(theta));
@@ -311,6 +311,7 @@ function render_sphere(position){
     var viewMatrix = lookAt(eye, at, up);
     projectionMatrix = ortho(left, right, bottom, ytop, near, far);
 
+    // translate the sphere to the given position
     var modelMatrix1 = translate(sphere_position[0], sphere_position[1], sphere_position[2]);
     modelViewMatrix = mult(viewMatrix, modelMatrix1);
     nMatrix = normalMatrix(modelViewMatrix, true);
