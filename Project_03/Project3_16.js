7ebe39bee8251b179c74daf0a7c8e91d0f28f645
McCormick95 <mccormick.ryan.95@gmail.com>
2024-10-20 01:30:59 -0400

    Focus Gaze working. Project_03 is complete. TODO: add comments and clean up code.

--- Project_03/Project3_16.js
+++ Project_03/Project3_16.js
@@ -27,7 +27,7 @@ var vb = vec4(0.0, 0.942809, 0.333333, 1);
 var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
 var vd = vec4(0.816497, -0.471405, 0.333333,1);
 
-var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
+var lightPosition = vec4(0.0, 0.25, 1.0, 0.0);
 var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
 var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
 var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
@@ -51,16 +51,30 @@ var eye;
 var at = vec3(0.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 
-var color_toggle = 0;
+// Awakening Variables
+var awaken_status = false;
+var color_one = vec4(0.0, 0.7, 0.3, 1.0);
+var color_two = vec4(0.8, 0.2, 0.4, 1.0);
 
-var color_one = vec4(0.8, 0.2, 0.4, 1.0);
-var color_two = vec4(0.0, 0.7, 0.3, 1.0);
-
+// Power Surge Variables
 var power_surge_status = false;
-var pulse_speed = 0.1;
+var pulse_speed = 0.007;
 var pulse_toggle = 1;
 var r_max, g_max, b_max;
 var updateDiffuse;
+
+// Seeking Light Variables
+var seeking_light_status = false;
+var update_light_pos;
+var light_speed = 0.05;
+var seek_toggle = 1;
+var new_light_pos;
+
+// Focused Gaze Variables
+var focused_gaze_status = false;
+var focused_gaze_speed = 0.02;
+var focused_gaze_toggle = 1;
+var temp_gaze;
 
 function triangle(a, b, c) {
      positionsArray.push(a);
@@ -121,7 +135,7 @@ window.onload = function init() {
     gl.useProgram(program);
 
     ambientProduct = mult(lightAmbient, materialAmbient);
-    diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    //diffuseProduct = mult(lightDiffuse, materialDiffuse);
     specularProduct = mult(lightSpecular, materialSpecular);
 
     tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
@@ -149,36 +163,29 @@ window.onload = function init() {
 
     // Awakening
     document.getElementById("Button0").onclick = function(){
-        if (color_toggle == 0){
-            materialDiffuse = color_two;
-            color_toggle = 1;
-        } else {
-            materialDiffuse = color_one;
-            color_toggle = 0;
-        }
-        materialDiffuse = mult(lightDiffuse, materialDiffuse);
-        updateDiffuseProduct(materialDiffuse);
+        awaken_status = !awaken_status;
+        awaken();
     };
     // Power Surge
     document.getElementById("Button1").onclick = function(){
         power_surge_status = !power_surge_status;
-        power_surge();
-        
+        power_surge(); 
     };
     // Seeking Light
     document.getElementById("Button2").onclick = function(){
-    
+        seeking_light_status = !seeking_light_status;
+        seeking_light();
     };
     // Focused Gaze
     document.getElementById("Button3").onclick = function(){
-
+        focused_gaze_status = !focused_gaze_status;
+        adjust_projection_matrix();
     };
-
 
     gl.uniform4fv(gl.getUniformLocation(program,"uAmbientProduct"),flatten(ambientProduct));
     updateDiffuseProduct(materialDiffuse);
     gl.uniform4fv(gl.getUniformLocation(program,"uSpecularProduct"),flatten(specularProduct));
-    gl.uniform4fv(gl.getUniformLocation(program,"uLightPosition"),flatten(lightPosition));
+    updateLightPosition(lightPosition);
     gl.uniform1f(gl.getUniformLocation(program,"uShininess"),materialShininess);
 
     render();
@@ -189,6 +196,21 @@ function updateDiffuseProduct(defuse) {
     gl.uniform4fv(gl.getUniformLocation(gl.getParameter(gl.CURRENT_PROGRAM), "uDiffuseProduct"), flatten(diffuseProduct));
 }
 
+function updateLightPosition(lightPos) {
+    lightPosition = lightPos;
+    gl.uniform4fv(gl.getUniformLocation(gl.getParameter(gl.CURRENT_PROGRAM), "uLightPosition"), flatten(lightPosition));
+}
+
+function awaken(){
+    if(awaken_status){
+        materialDiffuse = color_two;
+    }
+    else{
+        materialDiffuse = color_one;
+    }
+    updateDiffuseProduct(materialDiffuse);
+}
+
 function power_surge(){
     var color_temp = materialDiffuse;
     if (power_surge_status) {
@@ -197,9 +219,9 @@ function power_surge(){
         color_temp[1] += pulse_speed * pulse_toggle;
         color_temp[2] += pulse_speed * pulse_toggle;
 
-        color_temp[0] = Math.round(color_temp[0] * 10) / 10;  
-        color_temp[1] = Math.round(color_temp[1] * 10) / 10;
-        color_temp[2] = Math.round(color_temp[2] * 10) / 10;
+        color_temp[0] = Math.round(color_temp[0] * 1000) / 1000;  
+        color_temp[1] = Math.round(color_temp[1] * 1000) / 1000;
+        color_temp[2] = Math.round(color_temp[2] * 1000) / 1000;
 
         r_max = Math.min(1.0, color_temp[0]);
         g_max = Math.min(1.0, color_temp[1]);
@@ -214,7 +236,6 @@ function power_surge(){
         }
 
         updateDiffuse = vec4(color_temp[0], color_temp[1], color_temp[2], 1.0);
-
         updateDiffuseProduct(updateDiffuse); 
         
         requestAnimationFrame(power_surge);
@@ -224,13 +245,56 @@ function power_surge(){
     }
 }
 
+function seeking_light(){
+    var temp_light = lightPosition;
+
+    if(seeking_light_status){
+        temp_light[0] += light_speed * seek_toggle;
+
+        temp_light[0] = Math.round(temp_light[0] * 100) / 100;
+
+        if(temp_light[0] == 3.00 || temp_light[0] == -3.00){
+            seek_toggle *= -1;
+        }
+
+        new_light_pos = vec4(temp_light[0], lightPosition[1], lightPosition[2], 0.0);
+        updateLightPosition(new_light_pos);
+        requestAnimationFrame(seeking_light);
+    }
+    else{
+        return;
+    }
+}
+
+function adjust_projection_matrix(){
+    temp_gaze = projectionMatrix;
+
+    if(focused_gaze_status){
+        ytop += focused_gaze_speed * focused_gaze_toggle;
+        bottom -= focused_gaze_speed * focused_gaze_toggle;
+
+        ytop = Math.round(ytop * 100) / 100;
+        bottom = Math.round(bottom * 100) / 100;
+
+        console.log(ytop, bottom);
+
+        if(ytop == 9 || bottom == -9 || ytop == 3 || bottom == -3){
+            focused_gaze_toggle *= -1;
+        }
+        
+        requestAnimationFrame(adjust_projection_matrix);
+    }
+    else{
+        return;
+    }
+}
+
 // Define the positions for two spheres
-var position_1 = vec3(-1.5, 0, 0);  // Left sphere
-var position_2 = vec3(1.5, 0, 0);   // Right sphere
+var position_1 = vec3(-1.25, 0, 0);  // Left sphere
+var position_2 = vec3(1.25, 0, 0);   // Right sphere
 
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
     phi = dr;
     
     // Render spheres
